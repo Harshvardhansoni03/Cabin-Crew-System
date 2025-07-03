65ee19748a6527f0212080be8dd1154bb34574ef
Harshvardhan Soni <115218059+Harshvardhansoni03@users.noreply.github.com>
2025-07-04 00:12:18 +0530

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -4,16 +4,12 @@ module testbench;
     reg [1:0] sel;
     wire pwm1, pwm2, pwm3, pwm4, led_out;
 
-    // Clock generator
     always #5 clk = ~clk;
 
-    // Instantiate PWM Generators
     pwm_generator PWM1(clk, duty1, pwm1);
     pwm_generator PWM2(clk, duty2, pwm2);
     pwm_generator PWM3(clk, duty3, pwm3);
     pwm_generator PWM4(clk, duty4, pwm4);
-
-    // Instantiate MUX
     led_mux MUX(sel, pwm1, pwm2, pwm3, pwm4, led_out);
 
     initial begin
